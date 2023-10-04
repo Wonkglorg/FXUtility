f1e76fc0bcd2f4f3f960c43a2788849eed2b099f
Wonkglorg <Wonkglorg.18@gmail.com>
2023-10-04 13:20:35 +0200

    Added drag and pan classes

--- src/main/java/com/wonkglorg/fxutility/manager/SceneManager.java
+++ src/main/java/com/wonkglorg/fxutility/manager/SceneManager.java
@@ -12,68 +12,57 @@ import java.util.Map;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
-public class SceneManager
-{
-	private static SceneManager sceneManager;
-	private final Map<String, Scene> scenes = new HashMap<>();
-	private final Map<Class<? extends Node>, Map<String, ? super Node>> nodeMap = new HashMap<>();
-	private Stage primaryStage;
-	
-	public static SceneManager getInstance()
-	{
-		if(sceneManager == null)
-		{
-			sceneManager = new SceneManager();
-		}
-		return sceneManager;
-	}
-	
-	public void addScene(String name, String fxmlPath) throws IOException
-	{
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
-		Parent root = loader.load();
-		Scene scene = new Scene(root);
-		scenes.put(name, scene);
-	}
-	
-	public void loadScene(Stage primaryStage, String name)
-	{
-		Objects.requireNonNull(primaryStage);
-		primaryStage.setScene(scenes.get(name));
-		primaryStage.show();
-	}
-	
-	public void loadScene(String name)
-	{
-		loadScene(primaryStage, name);
-	}
-	
-	public void addNode(String name, String fxmlPathString) throws IOException
-	{
-		Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPathString)));
-		nodeMap.putIfAbsent(node.getClass(), new HashMap<>());
-		nodeMap.get(node.getClass()).put(name, node);
-		
-	}
-	
-	public <T extends Node> T getNode(String key, Class<T> clazz)
-	{
-		Node node = (Node) nodeMap.get(clazz).get(key);
-		if(clazz.isInstance(node))
-		{
-			return clazz.cast(node);
-		}
-		return null;
-	}
-	
-	public Stage getPrimaryStage()
-	{
-		return primaryStage;
-	}
-	
-	public void setPrimaryStage(Stage primaryStage)
-	{
-		this.primaryStage = Objects.requireNonNull(primaryStage);
-	}
-	
+public class SceneManager {
+    private static SceneManager sceneManager;
+    private final Map<String, Scene> scenes = new HashMap<>();
+    private final Map<Class<? extends Node>, Map<String, ? super Node>> nodeMap = new HashMap<>();
+    private Stage primaryStage;
+
+    public static SceneManager getInstance() {
+        if (sceneManager == null) {
+            sceneManager = new SceneManager();
+        }
+        return sceneManager;
+    }
+
+    public void addScene(String name, String fxmlPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scenes.put(name, scene);
+    }
+
+    public void loadScene(Stage primaryStage, String name) {
+        Objects.requireNonNull(primaryStage);
+        primaryStage.setScene(scenes.get(name));
+        primaryStage.show();
+    }
+
+    public void loadScene(String name) {
+        loadScene(primaryStage, name);
+    }
+
+    public void addNode(String name, String fxmlPathString) throws IOException {
+        Node node = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxmlPathString)));
+        nodeMap.putIfAbsent(node.getClass(), new HashMap<>());
+        nodeMap.get(node.getClass()).put(name, node);
+
+    }
+
+    public <T extends Node> T getNode(String key, Class<T> clazz) {
+        Node node = (Node) nodeMap.get(clazz).get(key);
+        if (clazz.isInstance(node)) {
+            return clazz.cast(node);
+        }
+        return null;
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = Objects.requireNonNull(primaryStage);
+    }
+
 }
