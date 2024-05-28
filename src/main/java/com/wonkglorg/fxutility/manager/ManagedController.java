4380a40276f09ea9d055799c3fd93a5553a0535c
Wonkglorg <Wonkglorg.18@gmail.com>
2024-05-28 19:13:37 +0200

    Added Init to ManagedController.java

--- src/main/java/com/wonkglorg/fxutility/manager/ManagedController.java
+++ src/main/java/com/wonkglorg/fxutility/manager/ManagedController.java
@@ -1,5 +1,10 @@
 package com.wonkglorg.fxutility.manager;
 
+import javafx.fxml.FXML;
+
+/**
+ * This class is a controller that is managed by the ManagedControllerManager. It is used to update the controller. and provide some useful methods.
+ */
 public abstract class ManagedController {
 
     /**
@@ -7,5 +12,14 @@ public abstract class ManagedController {
      */
     public abstract void update();
 
+    @FXML
+    void initialize() {
+        init();
+    }
+
+    /**
+     * This method is called after the FXML has been injected. Use this instead of a constructor to access fxml elements.
+     */
+    public abstract void init();
 
 }
