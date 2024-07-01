7a8c0f79efb3322df111989cbfbe255f2f9f8894
Wonkglorg <Wonkglorg.18@gmail.com>
2024-07-01 09:09:50 +0200

    Added Documentation for the ChooserUtil.java class

--- src/main/java/com/wonkglorg/fxutility/manager/util/ChooserUtil.java
+++ src/main/java/com/wonkglorg/fxutility/manager/util/ChooserUtil.java
@@ -20,34 +20,13 @@ public class ChooserUtil {
         this.stage = stage;
     }
 
-
     /**
      * Open a file chooser dialog
      *
-     * @param defaultDirectory The default directory to open the dialog in
-     * @param title            The title of the dialog
-     * @return The selected file
+     * @param fileType The file type filter
+     * @return The selected file or empty if no file was selected
      */
-    public Optional<File> directoryChooser(File defaultDirectory, String title) {
-        DirectoryChooser chooser = new DirectoryChooser();
-        chooser.setTitle(title);
-        chooser.setInitialDirectory(defaultDirectory);
 
-        File selectedDirectory = chooser.showDialog(stage);
-
-        if (selectedDirectory == null || !selectedDirectory.isDirectory()) {
-            return Optional.empty();
-        }
-        return Optional.of(selectedDirectory);
-    }
-
-    /**
-     * Open a file chooser dialog
-     *
-     * @param defaultDirectory The default directory to open the dialog in
-     * @param fileType         The file type filter
-     * @return The selected file
-     */
     public Optional<File> fileChooser(File defaultDirectory, String title, FileChooser.ExtensionFilter... fileType) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(fileType);
@@ -66,10 +45,80 @@ public class ChooserUtil {
      * Open a file chooser dialog
      *
      * @param defaultDirectory The default directory to open the dialog in
-     * @param title            The title of the dialog
-     * @param initialFileTitle The initial file title
      * @param fileType         The file type filter
      * @return The selected file
+     */
+    public Optional<File> fileChooser(File defaultDirectory, FileChooser.ExtensionFilter... fileType) {
+        return fileChooser(defaultDirectory, "Select File", fileType);
+    }
+
+    /**
+     * Open a file chooser dialog in the user home directory with a default "Select File" title
+     *
+     * @param fileType All allowed file types
+     * @return A file or empty if no file was selected
+     */
+    public Optional<File> fileChooser(FileChooser.ExtensionFilter... fileType) {
+        return fileChooser(new File(System.getProperty("user.home")), "Select File", fileType);
+    }
+
+    /**
+     * Open a file chooser dialog in the default user directory with no file type filter set
+     *
+     * @return A file or empty if no file was selected
+     */
+    public Optional<File> fileChooser() {
+        return fileChooser(new File(System.getProperty("user.home")), "Select File");
+    }
+
+
+    /**
+     * Open a file chooser dialog
+     *
+     * @param defaultDirectory Default directory to open the dialog in
+     * @param title            Title of the Dialog window
+     * @return A file representing a directory or empty if no directory was selected
+     */
+    public Optional<File> directoryChooser(File defaultDirectory, String title) {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle(title);
+        chooser.setInitialDirectory(defaultDirectory);
+
+        File selectedDirectory = chooser.showDialog(stage);
+
+        if (selectedDirectory == null || !selectedDirectory.isDirectory()) {
+            return Optional.empty();
+        }
+        return Optional.of(selectedDirectory);
+    }
+
+
+    /**
+     * Open a directory chooser dialog
+     *
+     * @param defaultDirectory Default directory to open the dialog in
+     * @return A file representing a directory or empty if no directory was selected
+     */
+    public Optional<File> directoryChooser(File defaultDirectory) {
+        return directoryChooser(defaultDirectory, "Select Directory");
+    }
+
+    /**
+     * Open a file chooser dialog in the default user directory
+     *
+     * @return A file representing a directory or empty if no directory was selected
+     */
+    public Optional<File> directoryChooser() {
+        return directoryChooser(new File(System.getProperty("user.home")));
+    }
+
+    /**
+     * Open a file chooser dialog
+     *
+     * @param defaultDirectory Default directory to open the dialog in
+     * @param fileType         All allowed file types
+     * @param title            The title of the dialog
+     * @return A file or empty if no file was selected
      */
     public Optional<File> saveDialog(File defaultDirectory, String title, String initialFileTitle, FileChooser.ExtensionFilter... fileType) {
         FileChooser fileChooser = new FileChooser();
@@ -85,61 +134,11 @@ public class ChooserUtil {
         return Optional.ofNullable(file);
     }
 
-    /**
-     * Open a file chooser dialog
-     *
-     * @param defaultDirectory The default directory to open the dialog in
-     * @param fileType         The file type filter
-     * @return The selected file
-     */
-    public Optional<File> fileChooser(File defaultDirectory, FileChooser.ExtensionFilter... fileType) {
-        return fileChooser(defaultDirectory, "Select File", fileType);
-    }
-
-    /**
-     * Open a file chooser dialog in the default user home directory
-     *
-     * @param fileType The file type filter
-     * @return The selected file
-     */
-    public Optional<File> fileChooser(FileChooser.ExtensionFilter... fileType) {
-        return fileChooser(new File(System.getProperty("user.home")), "Select File", fileType);
-    }
-
-    /**
-     * Open a file chooser dialog in the default user directory with no file type filter set
-     *
-     * @return The selected file
-     */
-    public Optional<File> fileChooser() {
-        return fileChooser(new File(System.getProperty("user.home")), "Select File");
-    }
-
-
-    /**
-     * Open a file chooser dialog
-     *
-     * @param defaultDirectory The default directory to open the dialog in
-     * @return The selected file
-     */
-    public Optional<File> directoryChooser(File defaultDirectory) {
-        return directoryChooser(defaultDirectory, "Select Directory");
-    }
-
-    /**
-     * Open a file chooser dialog in the default user directory
-     *
-     * @return The selected file
-     */
-    public Optional<File> directoryChooser() {
-        return directoryChooser(new File(System.getProperty("user.home")));
-    }
-
 
     /**
      * Sets the stage for the ChooserUtil
      *
-     * @param stage The stage to set
+     * @param stage
      */
     public void setStage(Stage stage) {
         this.stage = stage;
